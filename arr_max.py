ff2c8bfce19dcd259a25be00ae6a46b07bf1a649
seungmin <seungmin3888@google.com>
2022-11-10 06:12:29 +0900

    배열 원소의 최댓값을 구하는 함수 구현

---
+++ arr_max.py
@@ -0,0 +1,19 @@
+import typing
+
+def max_of(a: typing.Sequence)->typing.Any :
+    """시퀀스형 a 요소의 최댓값을 반환"""
+    maximum = a[0]
+    for i in range(1, len(a)):
+         if a[i] > maximum: 
+            maximum = a[i]
+    return maximum
+
+if __name__ == '__main__':
+    print('배열의 최댓값을 구합니다.')
+    num = int(input('원소 수를 입력하세요 : '))
+    x = [None] * num    # 원소 수가 num인 리스트를 생성
+
+    for i in range(num):
+        x[i] = int(input(f'x[{i}]를 입력하세요.: '))
+
+    print(f'최댓값은 {max_of(x)}입니다.')
